f5b6013fb364670252a1105aca44004c2d07d94e
Your Name <you@example.com>
2024-06-09 23:13:02 +0500

    first commit

---
+++ src/router/router.jsx
@@ -0,0 +1,12 @@
+const routes = [
+    {
+        path: "/main",
+        content: "Users",
+    },
+    {
+        path: "/main/product",
+        content: "Product",
+    },
+   
+]
+export default routes;
